6269bd771b1b0f5037a61e99ef153d0f188db453
thevu4496 <ts2a1996@gmail.com>
2021-02-21 20:52:35 +0700

    xxxx

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { db } from "./services/firebase";
+import io from 'socket.io-client';
 import {
   Route,
   BrowserRouter as Router,
@@ -59,6 +60,9 @@ class App extends Component {
         </Switch>
       </Router>
     );
+  }
+  componentWillMount(){
+    this.socket = io('https://nbchatserver4.herokuapp.com/');
   }
   componentDidMount() {
   auth().onAuthStateChanged((user) => {
